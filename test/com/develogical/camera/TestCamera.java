a5481275d36ca21d330fda0d81512394d405faf6
jslezacek <jslezacek@gmail.com>
2012-06-27 14:18:12 +0100

    Added shutterButton (online) mock test

--- test/com/develogical/camera/TestCamera.java
+++ test/com/develogical/camera/TestCamera.java
@@ -2,6 +2,7 @@ package com.develogical.camera;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.Sequence;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +14,9 @@ public class TestCamera {
     Mockery context = new Mockery();
 
     Sensor s = context.mock(Sensor.class);
+    MemoryCard m = context.mock(MemoryCard.class);
     Camera c = new Camera(s);
+    byte[] testarray = new byte[0];
 
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
@@ -47,13 +50,11 @@ public class TestCamera {
 
     @Test
     public void pressShutterWhenPowerOnCopiesData() {
-        context.checking(new Expectations() {{
-            never(s);
-        }
-        });
-
-        c.pressShutter();
+            context.checking(new Expectations() {{
+                exactly(1).of(s).readData(); will(returnValue(testarray));
+                exactly(1).of(m).write(testarray);
+            }
+            });
+            c.pressShutter();
     }
-
-
 }
